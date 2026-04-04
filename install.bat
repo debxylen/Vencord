130c47067063c7fdb1d28fae7cb1192265118223
Xylen <Priyanshudeb3@gmail.com>
2026-04-04 20:29:06 +0800

    install launhes discrod

--- install.bat
+++ install.bat
@@ -1,2 +1,15 @@
 @echo off
-pnpm build && pnpm inject -branch auto
+setlocal enabledelayedexpansion
+
+call pnpm build
+call pnpm inject -branch auto
+
+set "DISCORD_DIR=%LOCALAPPDATA%\Discord"
+
+for /f "delims=" %%i in ('dir "%DISCORD_DIR%\app-*" /b /ad-h /o-n') do (
+    set "LATEST=%%i"
+    goto :run
+)
+
+:run
+start "" "%DISCORD_DIR%\!LATEST!\Discord.exe"
