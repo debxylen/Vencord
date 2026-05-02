af287d1c0c7f48779d607311f0348f623cdb4ff6
Xylen <Priyanshudeb3@gmail.com>
2026-05-03 02:20:01 +0800

    migrate margins import from utils to components

--- scan.bat
+++
@@ -1,5 +0,0 @@
-rg -n "useSettings\(" src
-rg -n "useSettings\(\[\[" src
-rg -n "useSettings\(\)" src
-rg -n "UseSettings<|ResolveUseSettings<|use: settings => useSettings" src/api/Settings.ts
-rg -n "\"[A-Za-z0-9_.*-]+\"" src | rg -n "useSettings\(\["
